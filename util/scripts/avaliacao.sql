4205614de5464f7e52ae62637dd5f59d46764f58
Gabriel Lima Brito da Silva <gabriellimab@gmail.com>
2022-12-08 21:03:57 -0300

    feat: create script sql

---
+++ util/scripts/avaliacao.sql
@@ -0,0 +1,14 @@
+CREATE TABLE avaliacao (
+    idAvaliacao int,
+    nomeEquipe varchar(255),
+    nomeProjeto varchar(255),
+    titulo int,
+    viabilidade int,
+    replicabilidade int,
+    inovacao int,
+    apresentacao int,
+    exibicao int,
+    observacao varchar(255),
+    notaFinal int
+);
+
